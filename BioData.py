fcf04b44099ddf3299d803c1f5433b1ff1311dd4
yang1823027121 <1823027121@qq.com>
2026-05-23 09:58:05 +0800

    Add files via upload

---
+++ BioData.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Hello My Name is YYF")
+
+st.header("About Me")
+
+st.image("111.jpg",caption="my photo",width=200)
+st.text("1. I like watching movies")
+st.text("2. I am a student")
+st.text("3. I am study in Taylor")
+
+st.success("Welcome tomy first Streamlit app")
